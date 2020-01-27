59c9a548c8b6d15571c866c947a6691a46a7f2a6
Brandon Becker <b_becker4@fanshaweonline.ca>
2020-01-27 15:02:18 -0500

    added save functionality

--- Prototype.gd
+++ Prototype.gd
@@ -9,6 +9,15 @@ onready var player = $Player
 func _ready():
 	pass # Replace with function body.
 
+func save():
+	var saveDictionary = {
+		"PlayerPosX": player.position.x,
+		"PlayerPosY": player.position.y
+	}
+	var file = File.new()
+	file.open("saveGame.json", File.WRITE)
+	file.store_line(to_json(saveDictionary))
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
