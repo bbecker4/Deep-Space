d1b7c8684b3e02b0037cc2dab3185dd0cd300370
Hamza Mian <hamza.mian@hotmail.com>
2020-01-28 12:36:32 -0500

    Added states to enemy object

--- Enemy.gd
+++ Enemy.gd
@@ -4,10 +4,38 @@ extends KinematicBody2D
 # var a = 2
 # var b = "text"
 
+enum AI_STATE { IDLE, WANDERING, PURSUING, EVADING, RELOADING };
+
+var state = AI_STATE.IDLE;
+
+func _idle():
+	pass
+	
+func _wander():
+	pass
+
+func _pursue():
+	pass
+
+func _evade():
+	pass
+	
+func _reload():
+	pass
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if (state == AI_STATE.IDLE):
+		_idle()
+	elif (state == AI_STATE.WANDERING):
+		_wander()
+	elif (state == AI_STATE.PURSUING):
+		_pursue()
+	elif (state == AI_STATE.EVADING):
+		_evade()
+	elif (state == AI_STATE.RELOADING):
+		_reload()
