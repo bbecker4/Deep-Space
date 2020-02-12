eb5ed013f0fa55f58b9c8eaf4ec8cfc290f9fd56
Hamza Mian <hamza.mian@hotmail.com>
2020-02-12 12:38:14 -0500

    Added pursue behaviour to the AI enemy

--- Enemy.gd
+++ Enemy.gd
@@ -9,18 +9,28 @@ enum AI_STATE { IDLE, WANDERING, PURSUING, EVADING, RELOADING };
 var state = AI_STATE.IDLE;
 
 var distance_to_circle = 1.0
+var player = null
+
+var velocity = Vector2(0,0)
+var max_velocity = 50.0
 
 # function to process idle behaviour
 func _idle():
-	state = AI_STATE.WANDERING
+	state = AI_STATE.PURSUING
 	
 # function to process wander behaviour
 func _wander():
 	pass
 
 # function to process pursue behaviour
-func _pursue():
-	pass
+func _pursue(delta):
+	var target = player.position
+	var desiredVelocity = (target - position).normalized() * max_velocity
+	var steer = desiredVelocity - velocity
+	velocity += steer * delta
+	if (velocity.length() > max_velocity):
+		velocity = velocity.normalized() * max_velocity
+	
 
 # function to process evade behaviour
 func _evade():
@@ -32,7 +42,7 @@ func _reload():
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	player = get_node("/root/Node2D/Player")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -41,8 +51,9 @@ func _process(delta):
 	elif (state == AI_STATE.WANDERING):
 		_wander()
 	elif (state == AI_STATE.PURSUING):
-		_pursue()
+		_pursue(delta)
 	elif (state == AI_STATE.EVADING):
 		_evade()
 	elif (state == AI_STATE.RELOADING):
 		_reload()
+	position += velocity * delta
