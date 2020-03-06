69158b997b438810d8825907fb97d77b6a561e7f
Hamza Mian <hamza.mian@hotmail.com>
2020-03-06 15:28:05 -0500

    Added optipng conversion to the automation script

--- PNGQUANT/Convert.py
+++ PNGQUANT/Convert.py
@@ -6,10 +6,19 @@ for fichier in filelist[:]: # filelist[:] makes a copy of filelist.
 
 import subprocess
 for file in filelist:
-    print("Converting " + str(file))
+    print("pngquant converting: " + str(file))
+    outFile = "\"./PNGQUANT/" + file + "\""
     FNULL = open(os.devnull, 'w')    #use this if you want to suppress output to stdout from the subprocess
-    args = "pngquant.exe --quality=65-80 \"PNG/" + file + "\""
+    args = "pngquant.exe --quality=65-80 \"PNG/" + file + "\" -o " + outFile
     subprocess.call(args, stdout=FNULL, stderr=FNULL, shell=False)
 
+
+for file in filelist:
+    print("OptiPNG converting: " + str(file))
+    FNULL = open(os.devnull, 'w')    #use this if you want to suppress output to stdout from the subprocess
+    args = "optipng.exe -o1 -dir OptiPNG \"PNG/" + file + "\""
+    subprocess.call(args, stdout=FNULL, stderr=FNULL, shell=False)
+
+print("")
 print("Done!")
-g = raw_input("Press ENTER to continue...")
+g = input("Press ENTER to continue...")
